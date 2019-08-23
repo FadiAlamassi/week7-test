d69bd6b78cf7b1548337b50191a306aeae840625
FadiAlamassi <alamassifadi@gmail.com>
2019-08-24 01:21:07 +0300

    create handler file to handle the cities endpoint and return the cities from the database as json file

---
+++ server/controllers/cities.js
@@ -0,0 +1,7 @@
+const getCities = require('../database/queries/getCities');
+
+exports.cities = (req, res,next) => {
+getCities()
+.then(result => res.send(result.rows))
+.catch(err => next(err));
+}
