922046d1b8295260ec03258e29223ef8630b6d0c
FadiAlamassi <alamassifadi@gmail.com>
2019-08-24 01:23:42 +0300

    add handlers to getCities and addCity

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const {cities} = require('./cities');
+const {addCity} = require('./addCity');
 
 const router = express.Router();
 
-
+router.use('/cities',cities);
+router.use('/add-city',addCity);
 module.exports = router;
