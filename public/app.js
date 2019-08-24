0b09d6358b74b406d7d81305213d8c5d0ec4d242
FadiAlamassi <alamassifadi@gmail.com>
2019-08-24 19:37:14 +0300

    Add option to delete citiy

--- public/app.js
+++ public/app.js
@@ -9,7 +9,30 @@ fetch("/cities")
       row.appendChild(name);
       const city = document.createElement("td");
       city.textContent = c.city;
+      const deleteCity = document.createElement("td");
       row.appendChild(city);
+      const deleteBtn = document.createElement("button");
+      deleteBtn.setAttribute("onclick","deleteCity(this)");
+      deleteBtn.setAttribute("id",c.id);
+      deleteBtn.classList.add('delete');
+      deleteBtn.textContent = 'X';
+      deleteCity.appendChild(deleteBtn);
+      row.appendChild(deleteCity);
       table.appendChild(row);
     });
-  });
+  })
+
+
+  const deleteCity = (city)=>{
+    var data = {cityID: city.id};
+    fetch('/deleteCity',{
+      method: 'POST',
+      body:JSON.stringify(data),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    // .then(() => alert('City Deleted Successfully'))
+    .then(() => location.reload())
+    .catch(error => console.error('Error:', error));
+  }
