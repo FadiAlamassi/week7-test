3ce791e26602aea49f6267ac7ef6cc2cd8d724d6
FadiAlamassi <alamassifadi@gmail.com>
2019-08-24 12:25:20 +0300

    test

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -2,20 +2,21 @@ const {Pool} = require('pg');
 require('env2')('config.env');
 
 let dbUrl =  '';
-if(process.env.NODE_ENV = 'development'){
-  dbUrl = process.env.DB_URL;
-}else if(process.env.NODE_ENV = 'test'){
+if(process.env.NODE_ENV = 'test'){
   dbUrl  = process.env.TEST_DB_URL;
+}else if(process.env.NODE_ENV = 'development'){
+  dbUrl = process.env.DB_URL;
 }else{
   dbUrl = process.env.DATABASE_URL;
 }
+
 if (!dbUrl) {
   throw new Error('Enviroment variable DB_URL must be set');
 }
 
 const options = {
   connectionString:dbUrl,
-  SSL:true
+  ssl:true,
 }
 
 module.exports = new Pool(options);
