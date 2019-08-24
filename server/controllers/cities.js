819160877034db8e6060ff6541925013efb3fb5e
FadiAlamassi <alamassifadi@gmail.com>
2019-08-24 12:35:30 +0300

    test

--- server/controllers/cities.js
+++ server/controllers/cities.js
@@ -2,8 +2,6 @@ const { getCities } = require('../database/queries/getCities');
 
 exports.cities = (req, res,next) => {
 getCities()
-.then(result => console.log(result.rows))
-.then(res.send('Hi'))
-// .then(result => res.send(result.rows))
+.then(result => res.send(result.rows))
 .catch(err => next(err));
 }
