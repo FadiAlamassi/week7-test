65270371afc96023dbc55135fe525a26a761c213
FadiAlamassi <alamassifadi@gmail.com>
2019-08-25 11:10:19 +0300

    test heroku

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -3,10 +3,10 @@ require('env2')('config.env');
 
 // let dbUrl =  process.env.DATABASE_URL;
 let dbUrl ='';
-if(process.env.NODE_ENV = 'development'){
-  dbUrl = process.env.DB_URL;
-}else{
+if(process.env.NODE_ENV = 'production'){
   dbUrl = process.env.DATABASE_URL;
+}else{
+  dbUrl = process.env.DB_URL;
 }
 
 if (!dbUrl) {
